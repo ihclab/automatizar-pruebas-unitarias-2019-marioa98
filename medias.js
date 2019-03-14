5f7b2a47e730f446b1233cc7b4db4e6455f3a44a
dcl18 <dcl18@iMac-de-Facultad-2.local>
2019-03-14 10:23:17 -0600

    Problema 00

---
+++ medias.js
@@ -0,0 +1,40 @@
+function mediaAritmetica(values){
+
+let media = 0;
+
+for(index = 0; index<values.length; index++){
+    media += values[index];
+}
+
+media = media/values.length;
+    return media;
+}
+
+function raizEnesima(x, n){
+    return Math.pow(x,(1/n));
+}
+
+function mediaGeometrica(values){
+    let media = 1;
+    for(index = 0; index < values.length; index++){
+        media *= values[index];
+    }
+    return(raizEnesima(media, values.length));
+
+}
+
+function mediaArmonica(values){
+
+    let media = 0;
+    for(index = 0; index<values.length;index++){
+        media += 1/values[index];
+    }
+
+    media = values.length/media;
+    return media;
+}
+
+let values = [2, 4, 6, 12, 18];
+console.log(mediaAritmetica(values));
+console.log(mediaGeometrica(values));
+console.log(mediaArmonica(values));
