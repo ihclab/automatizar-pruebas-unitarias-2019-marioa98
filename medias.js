428d19c6ef44f6133025fece2b1c83169742585c
dcl18 <dcl18@iMac-de-Facultad-2.local>
2019-03-19 10:34:45 -0600

    correcciones

--- medias.js
+++ medias.js
@@ -1,40 +1,44 @@
-function mediaAritmetica(values){
+ 
+ class Medias{
 
-let media = 0;
+    static mediaAritmetica(values){
 
-for(index = 0; index<values.length; index++){
-    media += values[index];
-}
-
-media = media/values.length;
-    return media;
-}
-
-function raizEnesima(x, n){
-    return Math.pow(x,(1/n));
-}
-
-function mediaGeometrica(values){
-    let media = 1;
-    for(index = 0; index < values.length; index++){
-        media *= values[index];
-    }
-    return(raizEnesima(media, values.length));
-
-}
-
-function mediaArmonica(values){
-
-    let media = 0;
-    for(index = 0; index<values.length;index++){
-        media += 1/values[index];
+        let media = 0;
+    
+        for(index = 0; index<values.length; index++){
+            media += values[index];
+        }
+    
+        media = media/values.length;
+        return media;
     }
 
-    media = values.length/media;
-    return media;
-}
+    static raizEnesima(x, n){
+        return Math.pow(x,(1/n));
+    }
 
-let values = [2, 4, 6, 12, 18];
-console.log(mediaAritmetica(values));
-console.log(mediaGeometrica(values));
-console.log(mediaArmonica(values));
+    mediaGeometrica(values){
+        let media = 1;
+        for(index = 0; index < values.length; index++){
+            media *= values[index];
+        }
+        return(raizEnesima(media, values.length));
+    
+    }
+
+    mediaArmonica(values){
+
+        let media = 0;
+        for(index = 0; index<values.length;index++){
+            media += 1/values[index];
+        }
+    
+        media = values.length/media;
+        return media;
+    }
+     
+ }
+// let values = [2, 4, 6, 12, 18];
+// console.log(mediaAritmetica(values));
+// console.log(mediaGeometrica(values));
+// console.log(mediaArmonica(values));
