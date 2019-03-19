fb499867da27b44f1d795dd742fa5844424a8829
dcl18 <dcl18@iMac-de-Facultad-2.local>
2019-03-19 10:01:49 -0600

    Problema 03

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
 const fs = require('fs');
 
 function readFile(){
+    let datos;
+
     fs.readFile('/Users/dcl18/Desktop/automatizar-pruebas-unitarias-2019-marioa98/CasosPrueba.txt','utf8', function(err, data){
-        console.log(data);
+        datos = data.split('\r\n');
+        datos.forEach(element => {
+            parseFile(element);
+        });
     });
+
+    
+}
+
+function parseFile(file){
+    let process = file.split(':');
+    console.log(process);
 }
 readFile();
